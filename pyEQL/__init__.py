01e58a70a34892cb262c8768a8b7a60a6716750e
Ryan Kingsbury <RyanSKingsbury@alumni.unc.edu>
2014-09-15 18:12:28 -0400

    Whitespace change

--- pyEQL/__init__.py
+++ pyEQL/__init__.py
@@ -1,11 +1,11 @@
 '''
-    pyEQL
-    =====
-    
-    pyEQL is a python package for computing chemical equilibria.
-    
-    :copyright: 2013-2014 by Ryan S. Kingsbury
-    :license: TBD
+pyEQL
+=====
+
+pyEQL is a python package for computing chemical equilibria.
+
+:copyright: 2013-2014 by Ryan S. Kingsbury
+:license: TBD
 '''
 from pyEQL.pyEQL import Solution
 from pyEQL.parameter import unit
