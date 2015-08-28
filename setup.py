f406f61fc1a6a557a7acdfeb112ba07e1a92cf67
Ryan S. Kingsbury <RyanSKingsbury@alumni.unc.edu>
2015-08-27 23:00:41 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.2.1',
+    version='0.2.2',
 
     description='A Python library for solution chemistry',
     long_description=long_description,
