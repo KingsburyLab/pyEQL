2162c82ed02beda4ddd7a0a5e98bc758112646ea
Ryan Kingsbury <rkingsbury@users.noreply.github.com>
2023-06-29 16:52:59 -0400

    update pyscaffold content to v4.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
     Setup file for pyEQL.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.0.2.
+    This file was generated with PyScaffold 4.5.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
