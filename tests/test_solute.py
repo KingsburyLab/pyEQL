8fb4ce899ad98245d6d9e819e8d75d5555e5ec71
Ryan <rkingsbury@users.noreply.github.com>
2023-08-28 10:24:35 -0400

    Solute: include factor in from_formula

--- tests/test_solute.py
+++ tests/test_solute.py
@@ -17,3 +17,6 @@ def test_from_formula():
     assert s.n_elements == 1
     assert s.oxi_state_guesses == ({"Mg": 2.0},)
     assert s.molecular_weight == "24.305 g/mol"
+    s2 = Solute.from_formula("O6")
+    assert s2.formula == "O3(aq)"
+    assert s2.molecular_weight == "47.9982 g/mol"
