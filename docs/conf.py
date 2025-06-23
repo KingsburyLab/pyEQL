354683a5b3ae51743d269f52f383d0ed3608f4b7
Ugochukwu Nwosu <ugognw@gmail.com>
2025-06-23 03:36:41 -0700

    Don't run sphinx-apidoc (~10 warnings)
    Running sphinx-apidoc automatically creates files with `autodoc` directives
    in `api/`. However, `automodule` and `autoclass` directives are already
    manually included in `class_solution.md` and `internal.md`.
    
    This also clears a sphinx error due to due entries for many properties
    since modules are documented twice (once in `api/pyEQL.rst` and again in
    `api/modules.rst`)
    
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -28,31 +28,13 @@ sys.path.insert(0, os.path.join(__location__, "../src"))
 # setup.py install" in the RTD Advanced Settings.
 # Additionally it helps us to avoid running apidoc manually
 
-try:  # for Sphinx >= 1.7
-    from sphinx.ext import apidoc
-except ImportError:
-    from sphinx import apidoc
-
 output_dir = os.path.join(__location__, "api")
 module_dir = os.path.join(__location__, "../src/pyEQL")
+
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
     pass
-
-try:
-    import sphinx
-
-    cmd_line = f"sphinx-apidoc --implicit-namespaces -f -o {output_dir} {module_dir}"
-
-    args = cmd_line.split(" ")
-    if tuple(sphinx.__version__.split(".")) >= ("1", "7"):
-        # This is a rudimentary parse_version to avoid external dependencies
-        args = args[1:]
-
-    apidoc.main(args)
-except Exception as e:
-    print(f"Running `sphinx-apidoc` failed!\n{e}")
 
 # -- General configuration ---------------------------------------------------
 
