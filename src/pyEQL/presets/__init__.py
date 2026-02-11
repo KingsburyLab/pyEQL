e70df63f14faf00f2df7417f4eba2aaec32b5ccf
Sui Xiong Tay <st2591@tiger3.princeton.edu>
2026-02-11 10:25:15 -0500

    removed N2 from ATMOSPHERE

--- src/pyEQL/presets/__init__.py
+++ src/pyEQL/presets/__init__.py
@@ -1,7 +1,9 @@
 # Partial pressures of reactive gases in atmosphere
 # Obtained from https://www.noaa.gov/jetstream/atmosphere on 12/15/25
 # (N2=78.084%, O2=20.946%, CO2=0.042%)
-ATMOSPHERE = {"N2": -0.1074379, "CO2": -3.3767507, "O2": -0.6788989}
+# "N2": -0.1074379 is removed from ATMOSPHERE as N2 is inert, and want to avoid reacting with O2 to form NO3-.
+# See related discussion on the PHREEQC forum:https://phreeqcusers.org/index.php?topic=2371.0
+ATMOSPHERE = {"CO2": -3.3767507, "O2": -0.6788989}
 
 # The amount, in moles, of an assemblage of pure phases that can react
 # reversibly with the aqueous phase.
