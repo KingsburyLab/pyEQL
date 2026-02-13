5ba25e8626a573b4108bb65937d6cab74af837ce
Sui Xiong Tay <st2591@tiger3.princeton.edu>
2026-02-13 12:07:32 -0500

    refactor phreeqc2026 tests

--- tests/test_engine_phreeqc.py
+++ tests/test_engine_phreeqc.py
@@ -391,7 +391,7 @@ def test_alkalinity():
 def test_equilibrate_2L():
     solution = Solution({"Cu+2": "1 umol/L", "O-2": "1 umol/L"}, volume="2 L", engine="phreeqc")
     solution.equilibrate(atmosphere=True)
-    assert np.isclose(solution.get_total_amount("Cu", "umol/L").magnitude, 0.9999, atol=1e-1)  # PHREEQCUI - 2e-06
+    assert np.isclose(solution.get_total_amount("Cu", "umol").magnitude, 1.9999, atol=1e-1)  # PHREEQCUI - 2e-06
 
 
 def test_equilibrate_unrecognized_component():
@@ -423,6 +423,6 @@ def test_equilibrate_with_atm():
     s1 = Solution({}, pH=7.0, volume="1 L", engine="phreeqc")
     s1.equilibrate(atmosphere=True)
     # PHREEQCUI final CO2, O2, and N2 concentrations were slightly adjusted for consistency with wrapper outputs
-    assert np.isclose(s1.get_amount("CO2(aq)", "mol/L").magnitude, 0.00001429, atol=1e-6)  # PHREQCUI - 1.429e-05
-    assert np.isclose(s1.get_amount("O2(aq)", "mol/L").magnitude, 0.0002683, atol=1e-6)  # PHREEQCUI - 2.683e-04
-    assert np.isclose(s1.get_amount("N2(aq)", "mol/L").magnitude, 0)  # PHREEQCUI - 0
+    assert np.isclose(s1.get_amount("CO2(aq)", "mol/kg").magnitude, 0.00001429, atol=1e-6)  # PHREQCUI - 1.429e-05
+    assert np.isclose(s1.get_amount("O2(aq)", "mol/kg").magnitude, 0.0002683, atol=1e-6)  # PHREEQCUI - 2.683e-04
+    assert np.isclose(s1.get_amount("N2(aq)", "mol/kg").magnitude, 0)  # PHREEQCUI - 0
