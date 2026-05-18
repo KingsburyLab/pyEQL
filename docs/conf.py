a90b629d4355d7f20a11a4aff0e8e78bcb12b153
YitongPan1 <119685432+YitongPan1@users.noreply.github.com>
2026-05-18 06:45:39 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -180,12 +180,13 @@ html_theme_options = {
     "repo_url": "https://github.com/KingsburyLab/pyEQL/",
     "repo_name": "pyEQL",
     # 'logo_icon': 'e798',
-    "nav_links": 
-    [{
-            "title": "Releases",
-            "href": "https://github.com/KingsburyLab/pyEQL/releases",
-            "internal": False,
-    },],
+    "social": [
+        {
+            "icon": "fontawesome/brands/github",
+            "link": "https://github.com/KingsburyLab/pyEQL/releases",
+            "name": "Releases",
+        }
+    ],
     "toc_title": "pyEQL: a python interface for water chemistry",
     "palette": { "primary": "blue", "accent": "light-blue" },
     "globaltoc_collapse": True,
